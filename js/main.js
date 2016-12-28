f4415a7a0a6726e318482051c426b87daa1c2f84
paola <paolatorobejar@gmail.com>
2016-12-28 12:50:39 -0500

    js terminado sencillo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+function calcular()
+{
+	var numeroa=parseInt(document.getElementById("a").value);
+	var numerob=parseInt(document.getElementById("b").value);
+	var suma=numeroa+numerob;
 
+	if(suma== 10 || numeroa == 10 || numerob == 10 ){
+		document.getElementById("salida").innerHTML=1;
+	}else{
+		document.getElementById("salida").innerHTML=0;
+	}
+
+}
